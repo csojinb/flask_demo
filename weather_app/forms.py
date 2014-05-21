771d18dc5ea276cfdea670d93ed99b812195698c
Clara Bennett <csojinb@gmail.com>
2014-05-21 18:15:33 -0400

    Modify form to use WTForms, with regex validation
    - Doesn't do anything with nonvalid inputs yet
    
    	modified:   weather_app/forms.py
    	modified:   weather_app/www/routes.py
    	modified:   weather_app/www/templates/index.html
    
    Conflicts:
    	weather_app/forms.py
    	weather_app/www/routes.py

---
+++ weather_app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import Form
+from wtforms import TextField
+from wtforms.validators import Regexp, DataRequired
+
+class LocationForm(Form):
+    zip_code = TextField('zip_code',
+        validators=[DataRequired(), Regexp(r'^\d{5}(?:[-\s]\d{4})?$')],
+        description = {'placeholder': 'Zip Code'})
--- weather_app/www/routes.py
+++ weather_app/www/routes.py
@@ -1,15 +1,20 @@
 from flask import Blueprint, render_template, request, url_for, redirect
 
+from forms import LocationForm
+
 blueprint = Blueprint('weather', __name__, template_folder='templates')
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def get_location():
-    if request.method == 'POST':
-        location = request.form['zip_code']
+
+    location_form = LocationForm(csrf_enabled=False)
+    if request.method == 'POST' and location_form.validate():
+        location = location_form.zip_code.data
+        print location
         return redirect(url_for('.show_location', location=location))
 
-    return render_template('index.html')
+    return render_template('index.html', form=location_form)
 
 @blueprint.route('/<location>')
 def show_location(location):
